3f6dd2c07064c1fde3d9e8ef031d8a55922a3970
yodanisesutantio <yodanise66@gmail.com>
2024-07-28 08:53:05 +0700

    okay, I think the create course form is ready

--- resources/views/admin-page/create-course.blade.php
+++ resources/views/admin-page/create-course.blade.php
@@ -26,14 +26,14 @@
                 </div>
                 {{-- Input Course_Thumbnail --}}
                 <div class="flex flex-col gap-2">
-                    <label for="course_thumbnail" class="cursor-pointer rounded-lg bg-custom-disabled-light/60 hover:bg-custom-disabled-light duration-200">
-                        <div class="flex flex-col items-center justify-center w-full rounded-lg h-60 bg-cover bg-center border-2 border-custom-grey border-dashed overflow-hidden" id="course_thumbnail_wrapper">
+                    <label for="course_thumbnail" class="cursor-pointer rounded-lg">
+                        <div class="relative flex flex-col items-center justify-center w-full rounded-lg h-60 bg-cover bg-center bg-custom-disabled-light/60 hover:bg-custom-disabled-light border-2 border-custom-grey border-dashed overflow-hidden duration-300" id="course_thumbnail_wrapper">
+                            <div class="absolute top-0 left-0 w-full h-full hover:bg-custom-dark/30 duration-300 hidden" id="course_thumbnail_overlay"></div>
                             <div class="flex flex-col items-center justify-center px-8 pt-5 pb-6" id="uploadInfo">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="#646464" d="M18 8a2 2 0 1 1-4 0a2 2 0 0 1 4 0"/><path fill="#646464" fill-rule="evenodd" d="M11.943 1.25h.114c2.309 0 4.118 0 5.53.19c1.444.194 2.584.6 3.479 1.494c.895.895 1.3 2.035 1.494 3.48c.19 1.411.19 3.22.19 5.529v.088c0 1.909 0 3.471-.104 4.743c-.104 1.28-.317 2.347-.795 3.235c-.21.391-.47.742-.785 1.057c-.895.895-2.035 1.3-3.48 1.494c-1.411.19-3.22.19-5.529.19h-.114c-2.309 0-4.118 0-5.53-.19c-1.444-.194-2.584-.6-3.479-1.494c-.793-.793-1.203-1.78-1.42-3.006c-.215-1.203-.254-2.7-.262-4.558c-.002-.473-.002-.973-.002-1.501v-.058c0-2.309 0-4.118.19-5.53c.194-1.444.6-2.584 1.494-3.479c.895-.895 2.035-1.3 3.48-1.494c1.411-.19 3.22-.19 5.529-.19m-5.33 1.676c-1.278.172-2.049.5-2.618 1.069c-.57.57-.897 1.34-1.069 2.619c-.174 1.3-.176 3.008-.176 5.386v.844l1.001-.876a2.3 2.3 0 0 1 3.141.104l4.29 4.29a2 2 0 0 0 2.564.222l.298-.21a3 3 0 0 1 3.732.225l2.83 2.547c.286-.598.455-1.384.545-2.493c.098-1.205.099-2.707.099-4.653c0-2.378-.002-4.086-.176-5.386c-.172-1.279-.5-2.05-1.069-2.62c-.57-.569-1.34-.896-2.619-1.068c-1.3-.174-3.008-.176-5.386-.176s-4.086.002-5.386.176" clip-rule="evenodd"/></svg>
                                 <p class="mt-4 mb-2 text-base text-center text-custom-grey"><span class="font-semibold">Tekan untuk memilih foto yang akan diupload</span> atau seret foto anda ke area ini</p>
                                 <p class="text-sm text-custom-grey text-center">Format yang didukung .jpg, .png, atau .webp (MAX. 2 MB)</p>
                             </div>
-                            <img id="course_thumbnail_preview" class="hidden w-full h-full object-cover">
                             <input id="course_thumbnail" type="file" class="hidden">
                             @error('course_thumbnail')
                                 <span class="text-custom-destructive">{{ $message }}</span>
@@ -155,18 +155,49 @@
         // Preview the Uploaded Thumbnail
         $('#course_thumbnail').on('change', function(event) {
             const file = event.target.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function(e) {
-                    $('#course_thumbnail_wrapper').css('background-image', 'url(' + e.target.result + ')'); // Update the background image
-                    $('#course_thumbnail_wrapper').removeClass('border-2 border-custom-grey border-dashed');
-                    $('#uploadInfo').addClass('hidden'); //Hide the uploadInfo
-                };
-                reader.readAsDataURL(file); // Read the file as a data URL
-            } else {
-                $('#course_thumbnail_wrapper').addClass('border-2 border-custom-grey border-dashed');
-                $('#uploadInfo').removeClass('hidden'); // Hide the uploadInfo
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                $('#course_thumbnail_wrapper').css('background-image', 'url(' + e.target.result + ')'); // Update the background image
+                $('#course_thumbnail_overlay').removeClass('hidden');
+                $('#course_thumbnail_wrapper').removeClass('border-2 border-custom-grey border-dashed');
+                $('#uploadInfo').addClass('hidden'); //Hide the uploadInfo
+            };
+            reader.readAsDataURL(file); // Read the file as a data URL
+        });
+
+        const dropArea = $('#course_thumbnail_wrapper');
+        // Prevent default browser behavior for drag/drop events
+        dropArea.on({
+            dragover: function(e) {
+                e.preventDefault();
+                $(this).addClass('bg-custom-disabled-light'); // Optional: Add hover styling
+            },
+            dragleave: function(e) {
+                e.preventDefault();
+                $(this).removeClass('bg-custom-disabled-light'); // Optional: Remove hover styling
+            },
+            drop: function(e) {
+                e.preventDefault();
+                $(this).removeClass('bg-custom-disabled-light'); // Optional: Remove hover styling
+
+                const file = e.originalEvent.dataTransfer.files[0];
+
+                if (file) {
+                    // Process the dropped file (e.g., display preview, upload)
+                    handleDroppedFile(file);
+                }
             }
         });
+
+        function handleDroppedFile(file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                $('#course_thumbnail_wrapper').css('background-image', 'url(' + e.target.result + ')'); // Update the background image
+                $('#course_thumbnail_overlay').removeClass('hidden');
+                $('#course_thumbnail_wrapper').removeClass('border-2 border-custom-grey border-dashed');
+                $('#uploadInfo').addClass('hidden'); //Hide the uploadInfo
+            };
+            reader.readAsDataURL(file); // Read the file as a data URL
+        }
     </script>
 @endsection
