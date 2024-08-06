85740f91fbec23ec7a48557593dd8cf1401ecef1
yodanisesutantio <yodanise66@gmail.com>
2024-08-06 10:54:10 +0700

    Edited CTA and added the documentation

--- resources/views/admin-page/admin-course.blade.php
+++ resources/views/admin-page/admin-course.blade.php
@@ -11,6 +11,8 @@
             <img src="img/blank-profile.webp" alt="profile-picture" class="w-20 lg:w-32 h-20 lg:h-32 rounded-full">
         @endif
     </div>
+    
+    {{-- Course Price Range. For marketing purpose --}}
     <div class="flex flex-col gap-1 mt-3">
         <h1 class="font-encode font-semibold text-center text-2xl lg:text-4xl text-custom-dark">{{ auth()->user()->fullname }}</h1>
         @if (auth()->user()->description)
@@ -20,18 +22,20 @@
         @endif
     </div>
 
-    <div class="flex flex-row w-full my-6">
+    {{-- Course Length Average. For student consideration --}}
+    <div class="flex flex-row w-full my-5">
         <div class="flex flex-col items-center justify-center border-r border-custom-grey px-2 py-1 w-1/2">
-            <h2 class="font-league font-semibold text-2xl text-center text-custom-dark">{{ $minCoursePrice }} - {{ $maxCoursePrice }}</h2>
+            <h2 class="font-league font-semibold text-[21px] text-center text-custom-dark">{{ $minCoursePrice }} - {{ $maxCoursePrice }}</h2>
             <p class="font-league font-normal text-[14px]/tight text-center text-custom-grey">Rentang Harga Kursus</p>
         </div>
         <div class="flex flex-col items-center justify-center px-2 py-1 w-1/2">
-            <h2 class="font-league font-semibold text-2xl text-center text-custom-dark">{{ $averageCourseLength }}x</h2>
+            <h2 class="font-league font-semibold text-[21px] text-center text-custom-dark">{{ $averageCourseLength }}x</h2>
             <p class="font-league font-normal text-[14px]/tight text-center text-custom-grey">Rata-Rata Pertemuan</p>
         </div>
     </div>
 
-    <div class="w-full py-3 rounded-lg cursor-pointer border border-custom-green lg:hover:bg-custom-grey/20 text-center lg:text-lg text-custom-green font-semibold duration-500"><a href="" >Lihat Daftar Siswa Aktif</a></div>
+    {{-- Kinda CTA --}}
+    <a href="admin-course/active-student-list"><div class="w-full py-3 rounded-lg cursor-pointer border border-custom-green lg:hover:bg-custom-grey/20 text-center lg:text-lg text-custom-green font-semibold duration-500">Lihat Daftar Siswa Aktif</div></a>
 
     @include('partials.footer')
 @endsection
