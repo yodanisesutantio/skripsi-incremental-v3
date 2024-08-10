aed644828e63ad994f65be562dc5cff9a19a3780
yodanisesutantio <yodanise66@gmail.com>
2024-08-10 22:45:18 +0700

    added documentation in course-progress blade views

--- resources/views/admin-page/course-progress.blade.php
+++ resources/views/admin-page/course-progress.blade.php
@@ -3,6 +3,7 @@
 @include('partials.navbar')
 
 @section('content')
+    {{-- Headers --}}
     <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-3xl lg:text-4xl/snug mt-5 lg:mt-10">{{ $enrollment->course->course_name }}</h1>
     <p class="text-custom-grey font-league font-medium text-lg/tight lg:text-2xl mt-1">Instruktur : {{ $enrollment->instructor->fullname }} &nbsp; | &nbsp; Siswa : {{ $enrollment->student->fullname }}</p>
     
@@ -10,6 +11,7 @@
         <div class="lg:col-span-2 bg-custom-white flex flex-col gap-5">
             {{-- Menu Button Groups --}}
             <div class="flex flex-col font-league text-custom-white gap-3 my-6 lg:my-8">
+                {{-- Propose New Schedule Button --}}
                 <a href="/choose-new-course-schedule" class="w-full h-24 lg:h-28 bg-cover bg-center rounded-xl cursor-pointer" style="background-image: url('{{ asset('img/Course-Schedule-BG.webp') }}');">
                     <div class="flex flex-col justify-end p-2.5 bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/20 to-70% w-full h-full rounded-xl lg:hover:bg-custom-dark/40 lg:hover:transition-colors lg:duration-500">
                         <h2 class="text-lg/snug lg:text-2xl/[1.5rem] font-semibold">Jadwal Kursus</h2>
@@ -18,12 +20,14 @@
                 </a>
                 <div class="grid grid-cols-2 gap-3">
                     <div class="flex flex-col gap-3">
+                        {{-- Read Theory Button --}}
                         <a href="/course-theory" class="w-full h-32 lg:h-44 bg-cover bg-center rounded-xl cursor-pointer" style="background-image: url('{{ asset('img/Guide-BG.jpg') }}')">
                             <div class="flex flex-col justify-end p-2.5 bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/20 to-70% w-full h-full rounded-xl lg:hover:bg-custom-dark/40 lg:hover:transition-colors lg:duration-500">
                                 <h2 class="text-lg/snug lg:text-2xl/[1.5rem] font-semibold">Baca Panduan</h2>
                                 <p class="text-sm/tight lg:text-base/[1.35rem] text-custom-white font-light">Ikuti langkah-langkah nya</p>
                             </div>
                         </a>
+                        {{-- Contact Other Parties Button --}}
                         <button type="button" id="button-open-contact-party" class="w-full h-32 lg:h-44 bg-cover bg-center rounded-xl cursor-pointer" style="background-image: url('{{ asset('img/Contact-Course-BG.webp') }}')">
                             <div class="flex flex-col justify-end p-2.5 bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/20 to-70% text-left w-full h-full rounded-xl lg:hover:bg-custom-dark/40 lg:hover:transition-colors lg:duration-500">
                                 <h2 class="text-lg/snug lg:text-2xl/[1.75rem] font-semibold">Hubungi Pihak Kursus</h2>
@@ -32,6 +36,7 @@
                         </button>
                     </div>
     
+                    {{-- Open Quiz Button --}}
                     <a href="/course-quiz" class="w-full bg-cover bg-center rounded-xl cursor-pointer" style="background-image: url('{{ asset('img/Quiz-BG.jpg') }}')">
                         <div class="flex flex-col justify-end p-2.5 bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/20 to-70% text-left w-full h-full rounded-xl lg:hover:bg-custom-dark/40 lg:hover:transition-colors lg:duration-500">
                             <h2 class="text-lg/snug lg:text-2xl/[2rem] font-semibold">Quiz</h2>
