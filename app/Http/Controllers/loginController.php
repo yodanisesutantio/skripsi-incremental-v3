0f9f30c609a5a45aaf4bce43d7771feacfe606a2
yodanisesutantio <yodanise66@gmail.com>
2024-08-12 11:41:10 +0700

    logic controller is documented

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -9,42 +9,62 @@ use Illuminate\Support\Facades\Session;
 
 class loginController extends Controller
 {
+    // Login Page Controller
     public function index() {
         return view('/login', [
             'pageName' => "Login | "
         ]);
     }
 
+    // Authentication Controller, Login Logic Handler
     public function authenticate(Request $request): RedirectResponse {
+        // Validation Rules
         $credentials = $request->validate([
             'username' => ['required'],
             'password' => ['required']
-        ],[
+        ],
+        
+        // Validation Error messages
+        [
             'username.required' => 'Kolom ini harus diisi',
             'password.required' => 'Kolom ini harus diisi',
         ]);
 
+        // Authentication Logic
         if (Auth::attempt($credentials)) {
+            // Get the User data for this current user
             $user = Auth::user();
-            $role = $user->role;
+            // Get the User Role from the User data 
+            $role = $user->role; 
           
-            $request->session()->regenerate();
+            // Create a Session
+            $request->session()->regenerate(); 
+            // Generate a flash message via Toastr after login successful
             $request->session()->flash('success', 'Login Berhasil');
           
+            // Redirect User to the designated dashboard. If their role is Admin, go to admin-index, if their role is Instructor, go to instructor-index, if their role is user(student), then go to user-index
             return redirect()->intended('/' . $role . '-index');
         }
 
+        // If authentication failed, return this error message
         return back()->withErrors([
             'username' => 'Periksa kembali username anda',
             'password' => 'Periksa kembali password anda',
-        ])->onlyInput('username');
+        ])->onlyInput('username'); // Give the value of the username input field back, but leave the password input blank
     }
 
+    // Logout Logic Handler
     public function logout() {
         // $message = 'Logout Berhasil!';
+
+        // Calls logout method from Auth by Laravels
         Auth::logout();
+        // Invalidate current users session
         request()->session()->invalidate();
+        // Generate new token to make it impossible, even if users tried to go back using the back button of the browser
         request()->session()->regenerateToken();
+
+        // Redirect to landing page
         return redirect('/');
     }
 }
