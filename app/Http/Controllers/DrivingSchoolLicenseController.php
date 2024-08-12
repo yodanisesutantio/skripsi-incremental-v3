fbb846ddb0459f08f9fee70451d834322f240055
yodanisesutantio <yodanise66@gmail.com>
2024-08-12 19:34:25 +0700

    drivingschoollicensecontroller is documented and also the instructorcontroller

--- app/Http/Controllers/DrivingSchoolLicenseController.php
+++ app/Http/Controllers/DrivingSchoolLicenseController.php
@@ -10,12 +10,17 @@ use Illuminate\Http\Request;
 
 class DrivingSchoolLicenseController extends Controller
 {
+    // Create a New Driving School License Logic Handler
     public function drivingSchoolLicenseCreate(Request $request) {
+        // Validation Rules
         $this->validate($request, [
             'licensePath' => 'required|mimes:jpeg,png,jpg,webp|max:2048',
             'startLicenseDate' => 'required',
             'endLicenseDate' => 'required',
-        ],[
+        ],
+        
+        // Validation Error Message
+        [
             'licensePath.required' => 'Silahkan Pilih File untuk Diunggah',
             'licensePath.mimes' => 'Format yang didukung adalah .jpg, .png, dan .webp',
             'licensePath.max' => 'Ukuran gambar maksimal adalah 2 MB',
@@ -23,36 +28,50 @@ class DrivingSchoolLicenseController extends Controller
             'endLicenseDate.required' => 'Kolom ini harus diisi',
         ]);
 
+        // Check if the incoming request has an uploaded license
         $fileName = null;
         if ($request->hasFile('licensePath')) {
+            // assign the uploaded file to $file variable
             $file = $request->file('licensePath');
+            // rename the file name to store it inside the database
             $fileName = time() . '.' . $file->getClientOriginalExtension();
+            // save the uploaded file to Laravel Storage System
             $file->storeAs('drivingSchoolLicense', $fileName);
         }
 
-        $newLicense = new DrivingSchoolLicense();        
+        // Create a new Driving School License in the same named Tables
+        $newLicense = new DrivingSchoolLicense();      
+        // instead of the file being stored in database, we save the filename of the file from Laravel Storage
         $newLicense->licensePath = $fileName;
+        // assign the value of the startLicenseDate attribute as per request
         $newLicense->startLicenseDate = $request['startLicenseDate'];
+        // assign the value of the endLicenseDate attribute as per request
         $newLicense->endLicenseDate = $request['endLicenseDate'];
+        // assign the value of the admin_id attribute by the currently authenticated user
         $newLicense->admin_id = Auth::id();
+        // Save the new array of data to DrivingSchoolLicense Tables
         $newLicense->save();
 
+        // Generate a flash message via Toastr to let user know that the process is successful
         $request->session()->flash('success', 'Izin Kursus Berhasil Ditambahkan, Admin Sistem Akan Memvalidasi Izin Anda Terlebih Dahulu!');
-
+        // Redirect user to List of Driving School License        
         return redirect()->intended('/admin-driving-school-license');
     }
 
+    // Driving School License Delete Logic Handler
     public function drivingSchoolLicenseDelete($id)
     {
+        // find the desired license match the incoming ID with the ID from DrivingSchoolLicense Tables
         $license = DrivingSchoolLicense::findOrFail($id);
     
-        // Optionally, delete the thumbnail from storage
+        // Delete the thumbnail from storage
         if ($license->licensePath) {
             Storage::delete('drivingSchoolLicense/' . $license->licensePath);
         }
 
+        // Delete the desired DrivingSchoolLicense
         $license->delete();
-
+        // Redirect Admin to List of Driving School License
         return redirect()->intended('/admin-driving-school-license');
     }
 }
--- app/Http/Controllers/instructorController.php
+++ app/Http/Controllers/instructorController.php
@@ -10,34 +10,49 @@ use Illuminate\Support\Facades\Storage;
 
 class instructorController extends Controller
 {
+    // Deactivate Instructor Logic Handler
     public function deactivateInstructor(Request $request) {
+        // find the Instructor by matching the user_id with the incoming request from User Tables
         $user = User::find($request->user_id);
 
+        // If user is found, do this data transaction
         if ($user) {
+            // Change the availability attribute value to 0
             $user->availability = 0;
+            // Save the new data
             $user->save();
     
+            // Return success response to JSON
             return response()->json(['success' => true]);
         }
     
+        // Return failed response to JSON when no user is found
         return response()->json(['success' => false], 400);
     }
 
+    // Activate Instructor Logic Handler
     public function activateInstructor(Request $request) {
+        // find the Instructor by matching the user_id with the incoming request from User Tables
         $user = User::find($request->user_id);
+
+        // If user is found, do this data transaction
         if ($user) {
+            // Change the availability attribute value to 0
             $user->availability = 1;
+            // Save the new data
             $user->save();
 
+            // Return success response to JSON
             return response()->json(['success' => true]);
         }
 
+        // Return failed response to JSON when no user is found
         return response()->json(['success' => false], 400);
     }
 
+    // Create new Instructor Form Logic Handler
     public function createInstructorLogic(Request $request) {
-        // dd($request->allFiles());
-
+        // Validation Rules
         $this->validate($request, [
             'certificatePath' => 'required|mimes:jpeg,png,jpg,webp|max:2048',
             'startCertificateDate' => 'required',
@@ -50,7 +65,10 @@ class instructorController extends Controller
             'phone_number' => 'required|max:20|unique:users',
             'password' => 'required|min:5|max:255|confirmed',
             'password_confirmation' => 'required|min:5|max:255',
-        ],[
+        ],
+        
+        // Validation Error Messages
+        [
             'certificatePath.required' => 'Silahkan pilih Sertifikat Instruktur untuk diunggah',
             'certificatePath.mimes' => 'Format yang didukung adalah .jpg, .png, dan .webp',
             'certificatePath.max' => 'Ukuran gambar maksimal adalah 2 MB',
@@ -77,66 +95,102 @@ class instructorController extends Controller
             'password_confirmation.max' => 'Password terlalu panjang',
         ]);
 
+        // Replace 0 or 62 from the first 2 number of the inputted phone_number with +62, so it is easier to add wa.me/ link
         $request['phone_number'] = preg_replace('/^(0|62)/', '+62', $request->input('phone_number'));
 
+        // Detect if the incoming request has an uploaded profile picture
         if ($request->hasFile('hash_for_profile_picture')) {
+            // assign the uploaded file to $file variable
             $file = $request->file('hash_for_profile_picture');
+            // rename the file name to store it inside the database
             $fileName = time() . '.' . $file->getClientOriginalExtension();
+            // save the uploaded file to Laravel Storage System
             $file->storeAs('profile_pictures', $fileName);
         }
         
+        // Create a new Instructor Data in User Tables
         $user = User::create([
+            // instead of the file being stored in database, we save the filename of the file from Laravel Storage
             'hash_for_profile_picture' => $fileName,
+            // assign the value of the fullname attribute as per request
             'fullname' => $request->fullname,
+            // assign the value of the username attribute as per request
             'username' => $request->username,
+            // assign the value of the password attribute as per request but, we crypted it first
             'password' => bcrypt($request->password),
+            // assign the value of the age attribute as per request
             'age' => $request->age,
+            // assign the value of the description attribute as per request
             'description' => $request->description,
+            // assign the value of the phone_number as it is already formatted from the request
             'phone_number' => $request->phone_number,
+            // set the value of availability attribute to 0 by default, since sys_admin need to validate it first
             'availability' => 0,
+            // assign the value of the admin_id attribute by the currently authenticated user
             'admin_id' => Auth()->id(),
         ]);
         
+        // Save the new array of data to User Tables
         $user->save();
 
+
+        // Detect if the incoming request has an uploaded certificate
         if ($request->hasFile('certificatePath')) {
+            // assign the uploaded file to $fileCertificate variable
             $fileCertificate = $request->file('certificatePath');
+            // rename the filename to store it inside the database
             $certificateFile = time() . '.' . $fileCertificate->getClientOriginalExtension();
+            // save the uploaded file to Laravel Storage System
             $fileCertificate->storeAs('instructor_certificate', $certificateFile);
         }
 
+        // Create a new Instructor Certificate Data in InstructorCertificate Tables
         $certificate = InstructorCertificate::create([
+            // instead of the file being stored in database, we save the filename of the file from Laravel Storage
             'certificatePath' => $certificateFile,
+            // assign the value of the startCertificateDate attribute as per request
             'startCertificateDate' => $request->startCertificateDate,
+            // assign the value of the endCertificateDate attribute as per request
             'endCertificateDate' => $request->endCertificateDate,
+            // assign the value of the instructor_id attribute from the newly added instructor above
             'instructor_id' => $user->id,
         ]);
 
+        // Save the new array of data to InstructorCertificate Tables
         $certificate->save();
 
+        // Generate a flash message via Toastr to let user know that the process is successful
         session()->flash('success', 'Instruktur Berhasil Ditambahkan!');
+        // Redirect user to List of Instructor Page
         return redirect()->intended('/admin-manage-instructor');
     }
 
+    // Delete Instructor Logic Handler
     public function deleteInstructor($id) {
+        // Find the instructor data by matching the incoming ID with the ID from User Tables
         $instructor = User::findOrFail($id);
     
-        // Optionally, delete the thumbnail from storage
+        // Delete the thumbnail from storage
         if ($instructor->hash_for_profile_picture) {
             Storage::delete('profile_picture/' . $instructor->hash_for_profile_picture);
         }
 
+        // Then Delete the data
         $instructor->delete();
 
-        // Check if the user has any remaining instructors
+        // Check if the owner/admin has any remaining instructors
         $remainingInstructors = User::where('admin_id', auth()->id())->count();
 
+        // When they had no more instructors, generate this flash message via Toastr
         if ($remainingInstructors == 0) {
             session()->flash('warning', 'Anda sudah tidak memiliki Instruktur lagi!');
-        } else {
+        } 
+        
+        // If they still had at least more than 1 instructors, generate this flash message via Toastr
+        else {
             session()->flash('success', 'Instruktur Berhasil Dihapus');
         }
-
+        // Redirect the admin to List of Instructor Page
         return redirect()->intended('/admin-manage-instructor');
     }
 }
