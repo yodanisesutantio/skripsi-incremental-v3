7afed2e1b15da9e891252e78112c4cc00a504cf6
yodanisesutantio <yodanise66@gmail.com>
2024-08-16 15:28:51 +0700

    I'm thinking of working the delete permanently driving school first, the incoming schedule in admin index still too complicated for me

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -6,13 +6,17 @@
     {{-- Greetings Element --}}
     <div class="flex flex-col my-8">
         <p class="text-custom-grey font-league font-medium text-lg/tight lg:text-2xl/tight">Hi, {{ auth()->user()->fullname }}</p>
-        <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Belum ada kursus berlangsung</h1>
-    </div>
+        {{-- @if () --}}
+            <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Kursus Mendatang</h1>
+        {{-- @else --}}
+            <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Belum ada Kursus Mendatang</h1>
+        {{-- @endif --}}
 
-    {{-- Ongoing Course, Hidden if there are no active course --}}
-    <div class="flex flex-col mb-8 lg:mb-11">
-        <p class="font-league text-center lg:text-xl my-3 lg:my-6">(Tidak ada kursus mendatang)</p>
-    </div>
+        {{-- Ongoing Course, Hidden if there are no active course --}}
+        <div class="flex flex-col mb-8 lg:mb-11">
+            <p class="font-league text-center lg:text-xl my-3 lg:my-6">(Tidak ada kursus mendatang)</p>
+        </div>
+    </div>    
 
     {{-- Schedule Interface --}}
     <div class="flex flex-col mb-8 lg:mb-11">
