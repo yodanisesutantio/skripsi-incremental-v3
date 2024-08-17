65d3261efd02008a26ef5200f1a043cb9fb3fbcc
yodanisesutantio <yodanise66@gmail.com>
2024-08-17 08:07:12 +0700

    current incoming schedule for mobile view is done

--- resources/views/home/admin.blade.php
+++ resources/views/home/admin.blade.php
@@ -4,18 +4,66 @@
 
 @section('content')
     {{-- Greetings Element --}}
-    <div class="flex flex-col my-8">
+    <div class="flex flex-col mt-5 mb-8">
         <p class="text-custom-grey font-league font-medium text-lg/tight lg:text-2xl/tight">Hi, {{ auth()->user()->fullname }}</p>
-        {{-- @if () --}}
-            <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Kursus Mendatang</h1>
-        {{-- @else --}}
-            <h1 class="text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Belum ada Kursus Mendatang</h1>
-        {{-- @endif --}}
+        @if ($incomingSchedule)
+            <h1 class="mb-3 text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Kursus Mendatang</h1>
+        @else
+            <h1 class="mb-3 text-custom-dark font-encode tracking-tight font-semibold text-xl/tight lg:text-4xl/tight">Belum ada Kursus Mendatang</h1>
+        @endif
 
-        {{-- Ongoing Course, Hidden if there are no active course --}}
-        <div class="flex flex-col mb-8 lg:mb-11">
-            <p class="font-league text-center lg:text-xl my-3 lg:my-6">(Tidak ada kursus mendatang)</p>
-        </div>
+        {{-- Incoming Course Card --}}
+        @if ($incomingSchedule)
+            <a href="{{ url('/admin-course-progress/' . $incomingSchedule->enrollment->student->fullname . '/' . $incomingSchedule->enrollment['id']) }}" class="w-full bg-custom-white-hover p-3 lg:p-5 rounded-xl overflow-hidden drop-shadow-lg lg:cursor-pointer lg:drop-shadow lg:hover:drop-shadow-lg duration-300">
+                <div class="flex flex-col gap-4">
+                    {{-- Student Information --}}
+                    <div class="flex flex-row gap-3 items-center">
+                        {{-- Student Profile Picture --}}
+                        @if ($incomingSchedule->enrollment->student->hash_for_profile_picture)
+                        <img src="{{ asset('storage/profile_pictures/' . $incomingSchedule->enrollment->student->hash_for_profile_picture) }}" alt="Instructor Profile Picture" class="w-12 lg:w-16 h-12 lg:h-16 rounded-full object-cover object-center">
+                        {{-- Student Blank Profile Picture --}}
+                        @else
+                        <img src="{{ asset('img/blank-profile.webp') }}" alt="Blank Instructor Profile Picture" class="w-12 lg:w-16 h-12 lg:h-16 rounded-full object-cover object-center">
+                        @endif
+
+                        <div class="flex flex-col lg:gap-1">
+                            {{-- Student Name --}}
+                            <h2 class="font-encode tracking-tight font-semibold text-[17px]/tight lg:text-xl/tight text-custom-dark">{{ $incomingSchedule->enrollment->student->fullname }}</h2>
+                            {{-- Instructor Name --}}
+                            <p class="font-league text-[15px]/tight lg:text-lg/tight text-custom-grey">Instruktur : {{ $incomingSchedule->enrollment->instructor->fullname }}</p>
+                        </div>
+                    </div>
+
+                    {{-- Course Name and Next Course Date and Time --}}
+                    <div class="flex flex-col gap-3">
+                        <div class="flex flex-col gap-1">
+                            <h3 class="font-encode tracking-tight font-semibold text-[21px]/tight lg:text-[25px]/tight text-custom-dark">{{ $incomingSchedule->enrollment->course->course_name }}</h3>
+                            <p class="font-league text-[17px]/tight lg:text-xl/tight text-custom-grey">Pertemuan ke-{{ $incomingSchedule->meeting_number }}</p>
+                        </div>
+                        <div class="flex flex-col lg:flex-row gap-2">
+                            {{-- Next Course Date --}}
+                            <div class="flex flex-row items-center w-fit gap-2 px-2 lg:px-2.5 py-1 lg:py-1.5 border border-custom-green/50 rounded-lg">
+                                {{-- Small icons --}}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="-mt-0.5" width="22" height="22" viewBox="0 0 24 24"><path fill="#24596A" d="M22 14v-2c0-.839 0-1.585-.013-2.25H2.013C2 10.415 2 11.161 2 12v2c0 3.771 0 5.657 1.172 6.828C4.343 22 6.229 22 10 22h4c3.771 0 5.657 0 6.828-1.172C22 19.657 22 17.771 22 14M7.75 2.5a.75.75 0 0 0-1.5 0v1.58c-1.44.115-2.384.397-3.078 1.092c-.695.694-.977 1.639-1.093 3.078h19.842c-.116-1.44-.398-2.384-1.093-3.078c-.694-.695-1.639-.977-3.078-1.093V2.5a.75.75 0 0 0-1.5 0v1.513C15.585 4 14.839 4 14 4h-4c-.839 0-1.585 0-2.25.013z"/></svg>
+                                <h4 class="font-league text-base/tight lg:text-lg/tight mt-1 text-custom-green font-medium line-clamp-1 whitespace-nowrap">{{ $incomingSchedule->formattedStartDate }}</h4>
+                            </div>
+                            {{-- Next Course Time --}}
+                            <div class="flex flex-row items-center w-fit gap-2 px-2 lg:px-2.5 py-1 lg:py-1.5 border border-custom-green/50 rounded-lg">
+                                {{-- Small icons --}}
+                                <svg xmlns="http://www.w3.org/2000/svg" class="-mt-0.5" width="22" height="22" viewBox="0 0 24 24"><path fill="#24596A" d="M17 3.34A10 10 0 1 1 2 12l.005-.324A10 10 0 0 1 17 3.34M12 6a1 1 0 0 0-1 1v5.026l.009.105l.02.107l.04.129l.048.102l.046.078l.042.06l.069.08l.088.083l.083.062l3 2a1 1 0 1 0 1.11-1.664L13 11.464V7a1 1 0 0 0-.883-.993z"/></svg>
+                                <h4 class="font-league text-base/tight lg:text-lg/tight mt-1 text-custom-green font-medium line-clamp-1 whitespace-nowrap">{{ $incomingSchedule->formattedStartTime }} - {{ $incomingSchedule->formattedEndTime }} WIB</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+
+        {{-- No Incoming Course --}}
+        @else
+            <div class="flex flex-col mb-8 lg:mb-11">
+                <p class="font-league text-center lg:text-xl my-3 lg:my-6">(Tidak ada kursus mendatang)</p>
+            </div>
+        @endif
     </div>    
 
     {{-- Schedule Interface --}}
