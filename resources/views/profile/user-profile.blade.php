a61116f0d94fc17b011264f5ec6bffb954be3eed
yodanisesutantio <yodanise66@gmail.com>
2024-09-14 22:07:44 +0700

    user profile is done, I think I can continue to work on its edit profile now

--- resources/views/profile/user-profile.blade.php
+++ resources/views/profile/user-profile.blade.php
@@ -28,7 +28,51 @@
             </div>
         </div>
 
-        
+        {{-- Menu --}}
+        <div class="grid grid-cols-2 lg:grid-cols-5 grid-rows-4 lg:grid-rows-2 w-full mt-6 lg:mt-14 mb-3 lg:mb-8 text-custom-white gap-3">
+            {{-- Edit Profile --}}
+            <a href="/user-profile/edit" class="lg:row-span-2 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/edit-profile.webp') }}')">
+                <div class="flex flex-col gap-0.5 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Ubah Profil</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Perbarui informasi personal anda</p>
+                </div>
+            </a>
+            {{-- Course Progress Detail Page --}}
+            <a href="#" class="lg:col-span-2 w-full h-32 lg:h-40 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/course-progress.webp') }}')">
+                <div class="flex flex-col gap-0.5 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Detail Progress Kursus</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Lacak kemajuan progress kursus anda!</p>
+                </div>
+            </a>
+            {{-- About App --}}
+            <a href="/about-app" class="w-full h-32 lg:h-40 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/about-app.webp') }}');">
+                <div class="flex flex-col gap-0.5 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Tentang Aplikasi</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Informasi tentang kami</p>
+                </div>
+            </a>
+            {{-- Register as Driving School --}}
+            <a href="#" class="row-span-2 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/add-driving-school.webp') }}')">
+                <div class="flex flex-col gap-1 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Penyedia Kursus Mengemudi?</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Jadi bagian dari KEMUDI</p>
+                </div>
+            </a>
+            {{-- Contact Us --}}
+            <a href="/contact-us" class="w-full h-32 lg:h-40 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/contact-us.webp') }}');">
+                <div class="flex flex-col gap-0.5 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Hubungi Kami</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Laporkan masalah</p>
+                </div>
+            </a>
+            {{-- Delete Account --}}
+            <div class="col-span-2 w-full h-32 lg:h-40 bg-cover bg-center rounded-xl lg:cursor-pointer" style="background-image: url('{{ asset('img/delete-account.webp') }}')" onclick="deleteConfirmation()">
+                <div class="flex flex-col gap-0.5 justify-end p-[10px] bg-gradient-to-t from-custom-dark/80 from-15% to-custom-dark/30 to-70% font-league w-full h-full rounded-xl lg:hover:bg-custom-dark-low lg:hover:transition-colors lg:duration-500">
+                    <h2 class="text-lg/tight lg:text-2xl/[1.7rem] font-semibold">Hapus Akun KEMUDI</h2>
+                    <p class="text-sm/none lg:text-base/[1.35rem] text-custom-white font-light">Hapus akun KEMUDI dan semua data terkait</p>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="mt-4">
