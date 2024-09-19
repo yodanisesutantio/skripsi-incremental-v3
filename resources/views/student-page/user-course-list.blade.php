c86815154c01b8a105c27a6c7641fb04d6d9ea92
yodanisesutantio <yodanise66@gmail.com>
2024-09-19 18:46:00 +0700

    the course-history-page is done

--- resources/views/student-page/user-course-list.blade.php
+++ resources/views/student-page/user-course-list.blade.php
@@ -44,7 +44,7 @@
                             {{-- Course Meeting Number --}}
                             <p class="font-league text-[17px]/tight lg:text-xl/tight text-custom-grey">Pertemuan ke-{{ $incomingCourse->schedule[0]->meeting_number }}</p>
                         </div>
-                        <div class="flex flex-col lg:flex-row gap-2">
+                        <div class="flex flex-col lg:flex-row gap-2 lg:gap-3">
                             {{-- Next Course Date --}}
                             <div class="flex flex-row items-center w-fit gap-2 px-2 lg:px-2.5 py-1 lg:py-1.5 border border-custom-green/50 rounded-lg">
                                 {{-- Small icons --}}
@@ -67,6 +67,64 @@
 
     {{-- Active License Section --}}
     <h2 class="text-xl lg:text-2xl/snug mt-6 mb-3 lg:mt-6 lg:mb-3 text-custom-dark font-encode tracking-tight font-semibold">Kursus Anda</h2>
+    @if ($enrolledCourse->isEmpty())
+        <p class="font-league text-center lg:text-xl my-20 lg:my-14">(Anda sedang tidak mengikuti kursus apapun)</p>
+    @else
+        <div class="flex flex-col lg:grid lg:grid-cols-2 gap-3 lg:gap-5">
+            {{-- Call every active student --}}
+            @foreach ($enrolledCourse as $pastCourse)
+            {{-- To open the course progress for each student --}}
+            <a href="{{ url('/user-course-progress/' . $pastCourse->student_real_name . '/' . $pastCourse['id']) }}" class="w-full bg-custom-white-hover p-3 lg:p-5 rounded-xl overflow-hidden drop-shadow-lg lg:cursor-pointer lg:drop-shadow lg:hover:drop-shadow-lg duration-300">
+                <div class="flex flex-col gap-4">
+                    {{-- Student Information --}}
+                    <div class="flex flex-row gap-3 items-center">
+                        {{-- Student Profile Picture --}}
+                        @if ($pastCourse->student_profile_picture)
+                        <img src="{{ asset('storage/enrollment/profile_pictures/' . $pastCourse->student_profile_picture) }}" alt="Instructor Profile Picture" class="w-12 lg:w-16 h-12 lg:h-16 rounded-full object-cover object-center">
+                        {{-- Student Blank Profile Picture --}}
+                        @else
+                        <img src="{{ asset('img/blank-profile.webp') }}" alt="Blank Instructor Profile Picture" class="w-12 lg:w-16 h-12 lg:h-16 rounded-full object-cover object-center">
+                        @endif
+    
+                        <div class="flex flex-col lg:gap-1">
+                            {{-- Student Name --}}
+                            <h2 class="font-encode tracking-tight font-semibold text-[17px]/tight lg:text-xl/tight text-custom-dark">{{ $pastCourse->student_real_name }}</h2>
+                            {{-- Instructor Name --}}
+                            <p class="font-league text-[15px]/tight lg:text-lg/tight text-custom-grey">Instruktur : {{ $pastCourse->instructor->fullname }}</p>
+                        </div>
+                    </div>
 
-    @include('partials.footer')
+                    {{-- Course Name and Next Course Date and Time --}}
+                    <div class="flex flex-col gap-3">
+                        <div class="flex flex-col gap-1">
+                            {{-- Course Name --}}
+                            <h3 class="font-encode tracking-tight font-semibold text-[21px]/tight lg:text-[25px]/tight text-custom-dark">{{ $pastCourse->course->course_name }}</h3>
+                            {{-- Course Meeting Number --}}
+                            <p class="font-league text-[17px]/tight lg:text-xl/tight text-custom-grey">Oleh : {{ $pastCourse->course->admin->fullname }}</p>
+                        </div>
+                        
+                        <div class="flex flex-row flex-wrap gap-2 lg:gap-3">
+                            {{-- Course Length --}}
+                            <div class="flex flex-row items-center w-fit gap-2 px-2.5 lg:px-4 py-1.5 lg:py-2 bg-custom-disabled-dark rounded-lg">
+                                <h4 class="font-league text-base/tight lg:text-lg/tight mt-0.5 lg:mt-0 text-custom-dark font-medium line-clamp-1 whitespace-nowrap">{{ $pastCourse->course->course_length }} Pertemuan</h4>
+                            </div>
+                            {{-- Course Price --}}
+                            <div class="flex flex-row items-center w-fit gap-2 px-2.5 lg:px-4 py-1.5 lg:py-2 bg-custom-disabled-dark rounded-lg">
+                                <h4 class="font-league text-base/tight lg:text-lg/tight mt-0.5 lg:mt-0 text-custom-dark font-medium line-clamp-1 whitespace-nowrap">Rp. {{ number_format($pastCourse->course->course_price, 0, ',', '.') }},-</h4>
+                            </div>
+                            {{-- Car Type --}}
+                            <div class="flex flex-row items-center w-fit gap-2 px-2.5 lg:px-4 py-1.5 lg:py-2 bg-custom-disabled-dark rounded-lg">
+                                <h4 class="font-league text-base/tight lg:text-lg/tight mt-0.5 lg:mt-0 text-custom-dark font-medium line-clamp-1 whitespace-nowrap">Transmisi : {{ $pastCourse->course->car_type === 'Both' ? 'Manual & Matic' : $pastCourse->course->car_type }}</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </a>
+            @endforeach
+        </div>
+    @endif
+
+    <div class="mt-6">
+        @include('partials.footer')
+    </div>
 @endsection
