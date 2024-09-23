0cd3db206b2f474d2357191a167dfe1b885a787a
yodanisesutantio <yodanise66@gmail.com>
2024-09-23 11:33:05 +0700

    finally, now the accordion button is ready, only the accordion content left

--- resources/views/student-page/user-payment.blade.php
+++ resources/views/student-page/user-payment.blade.php
@@ -18,11 +18,11 @@
                                 {{-- Accordion Button --}}
                                 <h2 class="font-medium text-lg/tight lg:text-2xl/tight relative z-10 p-4 bg-custom-white-hover drop-shadow rounded-lg">
                                     <button class="accordion-button flex flex-row justify-between items-center w-full text-left" type="button" aria-expanded="false" aria-controls="collapseOne">
-                                        <div class="flex flex-row gap-3">
+                                        <div class="flex flex-row items-center gap-3">
                                             {{-- Payment Vendor Icon --}}
-                                            <img src="{{ asset('img/payment-vendor-icon/Bank-' . $payment->payment_vendor . '.webp') }}" alt="Bank : {{ $payment->payment_vendor }}" class="w-8">
+                                            <img src="{{ asset('img/payment-vendor-icon/Bank-' . $payment->payment_vendor . '.webp') }}" alt="Bank : {{ $payment->payment_vendor }}" class="w-8 h-8 object-contain">
 
-                                            Bank {{ $payment->payment_vendor }}
+                                            <p class="font-league">Bank {{ $payment->payment_vendor }}</p>
                                         </div>
 
                                         {{-- Arrow Down --}}
@@ -36,7 +36,7 @@
                                 {{-- Accordion Content --}}
                                 <div id="collapseOne" class="bg-custom-green-hover rounded-b-lg relative z-0 -mt-1.5 text-base/tight lg:text-lg/tight pt-6 pb-5 px-4 hidden">
                                     <div class="flex flex-col gap-4 lg:gap-6">
-
+                                        
                                     </div>
                                 </div>
                             @endforeach
@@ -46,4 +46,21 @@
             </div>
         </div>
     </div>
+
+    {{-- jQuery JS --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script>
+        // Function to open Accordion
+        $('.accordion-button').click(function() {
+            const content = $(this).closest('h2').next('div');
+            const arrowDown = $(this).find('.arrow-down');
+            const arrowUp = $(this).find('.arrow-up');
+
+            content.toggleClass('hidden');
+            arrowDown.toggleClass('hidden');
+            arrowUp.toggleClass('hidden'); 
+
+            $(this).attr('aria-expanded', content.is(':visible')); 
+        });
+    </script>
 @endsection
