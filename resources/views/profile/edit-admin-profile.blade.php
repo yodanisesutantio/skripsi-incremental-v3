f4836d57ad2e1c2a1e2b53a9583977d55798504a
yodanisesutantio <yodanise66@gmail.com>
2024-09-28 15:32:13 +0700

    I've added the show and hide function like the password input for the FP Answer Input

--- resources/views/profile/edit-admin-profile.blade.php
+++ resources/views/profile/edit-admin-profile.blade.php
@@ -191,10 +191,17 @@
                                         <span class="text-custom-destructive">{{ $message }}</span>
                                     @enderror
                                 </div>
-                                {{-- Input Password Challenge Answer --}}
+                                {{-- Input FP Answer --}}
                                 <div class="flex flex-col gap-2">
-                                    <label for="fp_answer" class="font-semibold font-league text-lg lg:text-xl text-custom-grey">Jawaban<span class="text-custom-destructive">*</span></label>
-                                    <input type="text" name="fp_answer" id="fp_answer" placeholder="Nama Lengkap" class="p-4 font-league font-medium text-lg/[0] bg-custom-white-hover text-custom-secondary placeholder:#48484833 rounded-lg @error('fp_answer') border-2 border-custom-destructive @enderror" value="{{ $decryptedFpAnswer }}">
+                                    <label for="fp_answer" class="font-semibold font-league text-lg lg:text-xl text-custom-grey">Ketik Ulang Password Baru<span class="text-custom-destructive">*</span></label>
+                                    <div class="relative flex justify-end items-center">
+                                        <input type="password" name="fp_answer" id="fp_answer" placeholder="Masukkan Jawaban dari Pertanyaan diatas" class="relative py-4 pl-4 pr-10 w-full font-league font-medium text-lg/[0] bg-custom-white-hover text-custom-secondary placeholder:#48484833 rounded-lg @error('password') border-2 border-custom-destructive @enderror" value="{{ $decryptedFpAnswer }}">
+                                        <div class="eyeIcon absolute mr-3" onclick="showHideFpAnswer()">
+                                            <svg id="showFpAnswer" class="cursor-pointer" style="display: none;" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#495D6477" d="M9.75 12a2.25 2.25 0 1 1 4.5 0a2.25 2.25 0 0 1-4.5 0"/><path fill="#495D6477" fill-rule="evenodd" d="M2 12c0 1.64.425 2.191 1.275 3.296C4.972 17.5 7.818 20 12 20c4.182 0 7.028-2.5 8.725-4.704C21.575 14.192 22 13.639 22 12c0-1.64-.425-2.191-1.275-3.296C19.028 6.5 16.182 4 12 4C7.818 4 4.972 6.5 3.275 8.704C2.425 9.81 2 10.361 2 12m10-3.75a3.75 3.75 0 1 0 0 7.5a3.75 3.75 0 0 0 0-7.5" clip-rule="evenodd"/></svg>
+        
+                                            <svg id="hideFpAnswer" class="cursor-pointer" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#495D6477" fill-rule="evenodd" d="M1.606 6.08a1 1 0 0 1 1.313.526L2 7l.92-.394v-.001c0-.001 0 0 0 0l.003.009l.021.045c.02.042.051.108.094.194c.086.172.219.424.4.729a13.37 13.37 0 0 0 1.67 2.237a11.966 11.966 0 0 0 .59.592C7.18 11.8 9.251 13 12 13a8.706 8.706 0 0 0 3.22-.602c1.227-.483 2.254-1.21 3.096-1.998a13.053 13.053 0 0 0 2.733-3.725l.027-.058l.005-.011a1 1 0 0 1 1.838.788L22 7l.92.394l-.003.005l-.004.008l-.011.026l-.04.087a14.045 14.045 0 0 1-.741 1.348a15.368 15.368 0 0 1-1.711 2.256l.797.797a1 1 0 0 1-1.414 1.415l-.84-.84a11.81 11.81 0 0 1-1.897 1.256l.782 1.202a1 1 0 1 1-1.676 1.091l-.986-1.514c-.679.208-1.404.355-2.176.424V16.5a1 1 0 0 1-2 0v-1.544c-.775-.07-1.5-.217-2.177-.425l-.985 1.514a1 1 0 0 1-1.676-1.09l.782-1.203c-.7-.37-1.332-.8-1.897-1.257l-.84.84a1 1 0 0 1-1.414-1.414l.797-.797a15.406 15.406 0 0 1-1.87-2.519a13.457 13.457 0 0 1-.591-1.107a5.418 5.418 0 0 1-.033-.072l-.01-.021l-.002-.007l-.001-.002v-.001C1.08 7.395 1.08 7.394 2 7l-.919.395a1 1 0 0 1 .525-1.314" clip-rule="evenodd"/></svg>
+                                        </div>
+                                    </div>
                                     @error('fp_answer')
                                         <span class="text-custom-destructive">{{ $message }}</span>
                                     @enderror
@@ -520,6 +527,23 @@
             }
             
             $confirmPasswordInput.attr('type', $confirmPasswordInput.attr('type') === "password" ? "text" : "password");
+        }
+
+        // Show and Hide FP Answer Input
+        function showHideFpAnswer() {
+            const $showFpAnswer = $('#showFpAnswer');
+            const $hideFpAnswer = $('#hideFpAnswer');
+            const $fpAnswerInput = $('#fp_answer');
+
+            if ($fpAnswerInput.attr('type') === "password") {
+                $showFpAnswer.css('display', 'block');
+                $hideFpAnswer.css('display', 'none');
+            } else {
+                $showFpAnswer.css('display', 'none');
+                $hideFpAnswer.css('display', 'block');
+            }
+            
+            $fpAnswerInput.attr('type', $fpAnswerInput.attr('type') === "password" ? "text" : "password");
         }
 
         // Assist the user to avoid adding space in their username
