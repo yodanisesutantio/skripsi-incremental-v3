b8553bd170e841d50def81de1d642e778eaeb7fb
yodanisesutantio <yodanise66@gmail.com>
2024-11-25 17:40:56 +0700

    final tweak before going in, I've misredirect the back button on mobile for the course-details page

--- resources/views/course-details.blade.php
+++ resources/views/course-details.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex flex-col lg:flex-row">
         {{-- Back Button --}}
-        <a href="{{ url('/user-index') }}" class="flex absolute top-8 left-5 bg-custom-dark-low rounded-full z-20 p-3 lg:hidden">
+        <a href="{{ url()->previous() }}" class="flex absolute top-8 left-5 bg-custom-dark-low rounded-full z-20 p-3 lg:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="#EBF0F2" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M20 12H4m0 0l6-6m-6 6l6 6"/></svg>
         </a>
 
